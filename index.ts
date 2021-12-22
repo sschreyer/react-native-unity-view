21ce0611188bf4ab6c2413fa2e3eec8d960e51a0
sschreyer <samschreyer360@gmail.com>
2021-12-22 12:49:56 +1100

    Moved index.ts to make this a better package

---
+++ index.ts
@@ -0,0 +1,12 @@
+import UnityView from './src/UnityView'
+import { UnityModule, UnityViewMessage } from './src/UnityModule'
+import MessageHandler from './src/MessageHandler'
+
+export default UnityView
+
+export {
+    UnityView,
+    UnityModule,
+    MessageHandler,
+    UnityViewMessage
+} 
